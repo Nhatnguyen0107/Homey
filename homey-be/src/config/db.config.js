a9ef31d0a4a7916d08a7ab43693b980d5b944292
Nhatnguyen0107 <nguyenquocnhat2005@gmail.com>
2025-10-09 18:47:42 +0700

    update

--- homey-be/src/config/db.config.js
+++ homey-be/src/config/db.config.js
@@ -1,5 +1,5 @@
 export default {
-  url: process.env.DATABASE_URL || "mysql://root:nhat2005@localhost:3306/homey_db",
+  url: process.env.DATABASE_URL || "mysql://root:nguyenthien22112005@localhost:3306/homey_db",
   dialect: process.env.DATABASE_DIALECT || "mysql",
   pool: {
     max: parseInt(process.env.DB_POOL_MAX, 10) || 5,
