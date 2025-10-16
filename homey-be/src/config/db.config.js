fb73466d9f36cf82bbb4f34de5a394f55fad72d9
Thien <nguyenthien22112005@gmail.com>
2025-10-16 17:56:52 +0700

    s

--- homey-be/src/config/db.config.js
+++ homey-be/src/config/db.config.js
@@ -1,6 +1,6 @@
 export default {
   // url: process.env.DATABASE_URL || "mysql://root@95b3a0aff86e.ngrok-free.app/homey_db",
-  url: process.env.DATABASE_URL || "mysql://avnadmin:AVNS_jz8Mc0Jpqu1-isCn30B@mysql-1561d5a8-nguyenthien22112005-ff3e.d.aivencloud.com:27401/defaultdb?ssl-mode=REQUIRED",
+  url: process.env.DATABASE_URL || "mysql://root:nguyenthien22112005@localhost:3306/homey_db",
   dialect: process.env.DATABASE_DIALECT || "mysql",
   pool: {
     max: parseInt(process.env.DB_POOL_MAX, 10) || 5,
