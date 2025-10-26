22ed3f73bc7200212d015ac0321b56783401fdfc
Nhatnguyen0107 <nguyenquocnhat2005@gmail.com>
2025-10-26 13:08:02 +0700

    ss

--- homey-be/src/routes/booking.route.js
+++ homey-be/src/routes/booking.route.js
@@ -1,26 +1,14 @@
+
 import { Router } from "express";
 import BookingController from "../controllers/booking.controller.js";
-import { jwt } from "../middlewares/auth.js";
 
-const router = Router();
 const controller = new BookingController();
-
-// ADMIN
-router.get("/", jwt(), controller.getAllBookings.bind(controller));
-
-//  USER
-router.post("/", jwt(), controller.createBooking.bind(controller));
-router.get("/my-bookings", jwt(), controller.getUserBookings.bind(controller));
-router.put("/:id/cancel", jwt(), controller.cancelBooking.bind(controller));
-
-//  ADMIN ACTIONS
-router.put("/:id/confirm", jwt(), controller.confirmBooking.bind(controller));
-router.put("/:id/complete", jwt(), controller.completeBooking.bind(controller));
+const router = Router();
+// define the about route
+router.get("/", controller.getAllBookings);
+// router.post("/", controller.createBookings);
+// router.delete("/:id", controller.deleteBookings);
+// router.get("/:id", controller.getBookingsById);
+// router.put("/:id", controller.editBookings);
 
 export default router;
-
-
-// router.post("/", controller.createUser);
-// router.delete("/:id", controller.deleteUser);
-// router.get("/:id", controller.getUserById);
-// router.put("/:id", controller.editUser);
